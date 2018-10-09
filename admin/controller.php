021682fdd04eeba0d396555d82d150a8a19d3dba
antoniopetruzzella <a.petruzzella71@gmail.com>
2018-10-09 15:07:22 +0200

    primo

---
+++ admin/controller.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @package		Joomla.Tutorials
+ * @subpackage	Component
+ * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license		License GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// No direct access to this file
+defined('_JEXEC') or die;
+
+require_once 'models/libs/getid3/getid3.php';
+require_once 'models/libs/debugg/debugg.php';
+
+//require_once JPATH_COMPONENT.DS.'models'.DS.'libs'.DS.'debugg'.DS.'debugg.php';
+jimport('joomla.application.component.controller');
+
+class gglmsController extends JControllerLegacy {
+
+    public function __construct($config = array()) {
+        parent::__construct($config);
+
+
+    }
+
+    public function __destruct() {
+    }
+
+    function display($cachable = false, $urlparams = false) {
+        gglmsHelper::addSubmenu('messages');
+//        echo  $this->sidebar = JHtmlSidebar::render();  //RS
+        
+        JRequest::setVar('view', JRequest::getCmd('view', 'contents'));
+        parent::display($cachable);
+    }
+
+  
+
+}
---
+++ site/controller.php
@@ -0,0 +1,38 @@
+<?php
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+require_once 'administrator/components/com_gglms/models/libs/debugg/debugg.php';
+require_once JPATH_COMPONENT . '/helpers/output.php';
+require_once JPATH_COMPONENT . '/helpers/utility.php';
+require_once JPATH_COMPONENT . '/models/contenuto.php';
+require_once JPATH_COMPONENT . '/models/unita.php';
+require_once JPATH_COMPONENT . '/models/users.php';
+
+jimport('joomla.application.component.controller');
+jimport('joomla.access.access');
+
+
+class gglmsController extends JControllerLegacy {
+
+    private $_user;
+    private $_japp;
+    public  $_params;
+
+    public function __construct($config = array())
+    {
+        parent::__construct($config);
+
+        $this->_japp = JFactory::getApplication();
+
+
+        JHtml::_('jquery.framework');
+
+
+        JHtml::script(Juri::base() . 'components/com_gglms/libraries/js/mediaelement-and-player.js');
+        JHtml::script(Juri::base() . 'components/com_gglms/libraries/js/bootstrap.min.js');
+
+    }
+
+}
